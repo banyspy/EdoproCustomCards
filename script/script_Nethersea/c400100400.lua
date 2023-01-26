853a53b1c2d880d0720a280e324e256400ec2051
พิสิษฐ์ อัครธีรพิศาล <banyspy@gmail.com>
2023-01-26 23:39:04 +0700

    Add 2 onfield effect to brandguider; Fix opt issue of spewer

--- script/script_Nethersea/c400100400.lua
+++ script/script_Nethersea/c400100400.lua
@@ -24,6 +24,27 @@ function s.initial_effect(c)
 	e1:SetTarget(s.sumtg)
 	e1:SetOperation(s.sumop)
 	c:RegisterEffect(e1)
+	--change to def to negate all faceup your opponent card and increase own def
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_DISABLE+CATEGORY_DEFCHANGE)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetCondition(s.negcon)
+	e2:SetTarget(s.negtg)
+	e2:SetOperation(s.negop)
+	c:RegisterEffect(e2)
+	--set s/t upon ss in def
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e3:SetCondition(s.setcon)
+	e3:SetTarget(s.settg)
+	e3:SetOperation(s.setop)
+	c:RegisterEffect(e3)
 
 	Nethersea.GenerateToken(c)
 end
@@ -43,5 +64,76 @@ function s.sumop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.Summon(tp,c,true,nil,1)
 	else
 		Duel.MSet(tp,c,true,nil,1)
+	end
+end
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPosition(POS_FACEUP_ATTACK)
+end
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingTarget(Card.IsNegatable,tp,0,LOCATION_ONFIELD,1,nil) end
+	local g = Duel.GetMatchingGroup(Card.IsNegatable,tp,0,LOCATION_ONFIELD,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,#g,0,0)
+end
+function s.negop(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c = e:GetHandler()
+	if (not c:IsRelateToEffect(e)) or c:IsDefensePos() then return end
+	Duel.ChangePosition(c,POS_FACEUP_DEFENSE)
+	local g = Duel.GetMatchingGroup(Card.IsNegatable,tp,0,LOCATION_ONFIELD,nil)
+	for tc in aux.Next(g) do
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e2)
+	end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_DEFENSE)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	e1:SetValue(c:GetAttack())
+	c:RegisterEffect(e1)
+end
+function s.cfilter(c,tp)
+	return c:IsSetCard(SET_NETHERSEA) and c:IsControler(tp) and c:IsDefensePos()
+end
+function s.setcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,e:GetHandler(),tp) and e:GetHandler():GetFlagEffect(0,CARD_NETHERSEA_BRANDGUIDER)==0
+end
+function s.setcheck(c)
+	return c:IsSetCard(SET_NETHERSEA) and c:IsSpellTrap() and c:IsSSetable()
+end
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.setcheck,tp,LOCATION_DECK,0,1,nil) end
+	e:GetHandler():RegisterFlagEffect(0,CARD_NETHERSEA_BRANDGUIDER,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE_CAL,0,1)
+end
+function s.setop(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectMatchingCard(tp,s.setcheck,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		local tg=g:GetFirst()
+		Duel.SSet(tp,tg)
+		if tg:IsType(TYPE_QUICKPLAY) then
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+			e1:SetCode(EFFECT_QP_ACT_IN_SET_TURN)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			e1:SetDescription(aux.Stringid(id,2))
+			tg:RegisterEffect(e1)
+		end
+		if tg:IsType(TYPE_TRAP) then
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+			e1:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			e1:SetDescription(aux.Stringid(id,2))
+			tg:RegisterEffect(e1)
+		end
 	end
 end

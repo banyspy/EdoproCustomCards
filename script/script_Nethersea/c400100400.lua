53158264c712aa154eef9b81d7efb1ab3b2b68ee
พิสิษฐ์ อัครธีรพิศาล <banyspy@gmail.com>
2023-02-04 01:58:37 +0700

    Change Effect (in desc)
    Brandguider set effect has been changed

--- script/script_Nethersea/c400100400.lua
+++ script/script_Nethersea/c400100400.lua
@@ -7,6 +7,7 @@ function s.initial_effect(c)
 	Nethersea.QuickTributeProc(c)
 	--change to def to negate all faceup your opponent card and increase own def
 	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_DISABLE+CATEGORY_DEFCHANGE)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
@@ -17,16 +18,24 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 	--set s/t upon ss in def
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,1))
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
 	e3:SetRange(LOCATION_MZONE)
-	e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
-	e3:SetCountLimit(1)
 	e3:SetCondition(s.setcon)
+	e3:SetCountLimit(1)
 	e3:SetTarget(s.settg)
 	e3:SetOperation(s.setop)
 	c:RegisterEffect(e3)
+	--For reset flag of set s/t effect
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_DELAY+EFFECT_FLAG_UNCOPYABLE)
+	e4:SetCode(EVENT_TURN_END)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCondition(s.resetcon)
+	e4:SetOperation(s.reset)
+	c:RegisterEffect(e4)
 
 	Nethersea.GenerateToken(c)
 end
@@ -34,9 +43,10 @@ function s.negcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsPosition(POS_FACEUP_ATTACK)
 end
 function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chk==0 then return Duel.IsExistingTarget(Card.IsNegatable,tp,0,LOCATION_ONFIELD,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsNegatable,tp,0,LOCATION_ONFIELD,1,nil) end
 	local g = Duel.GetMatchingGroup(Card.IsNegatable,tp,0,LOCATION_ONFIELD,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,#g,0,0)
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
 end
 function s.negop(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local c = e:GetHandler()
@@ -62,41 +72,34 @@ function s.negop(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	e1:SetValue(c:GetAttack())
 	c:RegisterEffect(e1)
 end
-function s.cfilter(c,tp)
-	return c:IsSetCard(SET_NETHERSEA) and c:IsControler(tp) and c:IsDefensePos()
-end
 function s.setcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.cfilter,1,e:GetHandler(),tp)
+	return e:GetHandler():GetFlagEffect(tp,CARD_NETHERSEA_BRANDGUIDER) == 0
 end
 function s.setcheck(c)
 	return c:IsSetCard(SET_NETHERSEA) and c:IsSpellTrap() and c:IsSSetable()
 end
 function s.settg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.setcheck,tp,LOCATION_DECK,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.setcheck,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil) end
+	e:GetHandler():RegisterFlagEffect(tp,CARD_NETHERSEA_BRANDGUIDER,RESET_EVENT+RESETS_STANDARD,0,1)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(id,3))
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
+	e:GetHandler():RegisterEffect(e1)
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
 end
 function s.setop(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
-	local g=Duel.SelectMatchingCard(tp,s.setcheck,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.setcheck,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil)
 	if #g>0 then
 		local tg=g:GetFirst()
 		Duel.SSet(tp,tg)
-		if tg:IsType(TYPE_QUICKPLAY) then
-			local e1=Effect.CreateEffect(e:GetHandler())
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
-			e1:SetCode(EFFECT_QP_ACT_IN_SET_TURN)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-			e1:SetDescription(aux.Stringid(id,2))
-			tg:RegisterEffect(e1)
-		end
-		if tg:IsType(TYPE_TRAP) then
-			local e1=Effect.CreateEffect(e:GetHandler())
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
-			e1:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-			e1:SetDescription(aux.Stringid(id,2))
-			tg:RegisterEffect(e1)
-		end
 	end
+end
+function s.resetcon(e,tp,eg,ep,ev,re,r,rp)
+	return not Duel.IsTurnPlayer(tp)
+end
+function s.reset(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():ResetFlagEffect(tp,CARD_NETHERSEA_BRANDGUIDER)
 end

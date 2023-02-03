2aed9baf715f4140d94caee00a65c46cd5bf1407
พิสิษฐ์ อัครธีรพิศาล <banyspy@gmail.com>
2023-02-04 03:44:35 +0700

    Add Effect(in desc)
    Reefbreaker now can do quick attack against opponent monster

--- script/script_Nethersea/c400100600.lua
+++ script/script_Nethersea/c400100600.lua
@@ -5,24 +5,64 @@ Duel.LoadScript('NetherseaAux.lua')
 function s.initial_effect(c)
 	--Quick Tribute summon from hand
 	Nethersea.QuickTributeProc(c)
-	--gain attack when declare an attack
+	--Immediately attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
+	e2:SetCondition(s.atkcon)
+	e2:SetTarget(s.atktg)
+	e2:SetOperation(s.atkop)
+	c:RegisterEffect(e2)
+	--gains attack during damage calculation
 	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
 	e3:SetCategory(CATEGORY_ATKCHANGE)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
 	e3:SetRange(LOCATION_MZONE)
-	e3:SetTarget(s.atktg)
-	e3:SetOperation(s.atkop)
+	e3:SetTarget(s.gainatktg)
+	e3:SetOperation(s.gainatkop)
 	c:RegisterEffect(e3)
 	
 	Nethersea.GenerateToken(c)
 end
+function s.atkcon()
+	return Duel.IsMainPhase()
+end
+function s.atkcheck(c,card)
+	return c:IsCanBeBattleTarget(card)
+end
 function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return Duel.GetAttacker()==c and c:GetFlagEffect(id)==0 end
-	c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE_CAL,0,1)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.atkcheck,tp,0,LOCATION_MZONE,1,nil,c) and c:IsAttackPos() end
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsAttackPos() then return end
+	local g=Duel.SelectMatchingCard(tp,s.atkcheck,tp,0,LOCATION_MZONE,1,1,nil,c)
+	if #g>0 then
+		local tg=g:GetFirst()
+		Duel.HintSelection(tg)
+		local e1=Effect.CreateEffect(c)
+		e1:SetDescription(aux.Stringid(id,3))
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_NO_BATTLE_DAMAGE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE)
+		c:RegisterEffect(e1)
+		Duel.ForceAttack(c,tg)
+	end
+end
+function s.gainatktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetAttacker()==c and c:GetFlagEffect(CARD_NETHERSEA_REEFBREAKER)==0 end
+	c:RegisterFlagEffect(CARD_NETHERSEA_REEFBREAKER,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE_CAL,0,1)
+end
+function s.gainatkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and c:IsFaceup() then
 		local e1=Effect.CreateEffect(c)

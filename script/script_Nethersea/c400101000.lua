4b8f76f68e5183ec64e36f7072241225fcf4c6ad
พิสิษฐ์ อัครธีรพิศาล <banyspy@gmail.com>
2023-02-10 20:18:04 +0700

    Bug Fix(in desc)
    Approaching check activation legality incorrectly

--- script/script_Nethersea/c400101000.lua
+++ script/script_Nethersea/c400101000.lua
@@ -39,7 +39,7 @@ function s.spfilter(c,e,tp)
 	return Nethersea.NetherseaMonsterOrWQ(c) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.activatetarget(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,nil,e,tp) 
+	if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) 
 	 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
 end

c62c54aaf0a25e080e3239d20121a9664de23708
พิสิษฐ์ อัครธีรพิศาล <banyspy@gmail.com>
2023-02-11 18:23:58 +0700

    Effect change(in desc)
    Reefbreaker quick attack ffect now change to perform during chain link to simplify stuff though still has some weird thing going on

--- script/script_Nethersea/c400100600.lua
+++ script/script_Nethersea/c400100600.lua
@@ -6,44 +6,48 @@ function s.initial_effect(c)
 	--Quick Tribute summon from hand
 	Nethersea.QuickTributeProc(c)
 	--Immediately attack
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetCode(EVENT_FREE_CHAIN)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1)
-	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
-	e2:SetCondition(s.atkcon)
-	e2:SetTarget(s.atktg)
-	e2:SetOperation(s.atkop)
-	c:RegisterEffect(e2)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END+TIMING_END_PHASE)
+	e1:SetTarget(s.atktg)
+	e1:SetOperation(s.atkop)
+	c:RegisterEffect(e1)
 	--gains attack during damage calculation
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,2))
-	e3:SetCategory(CATEGORY_ATKCHANGE)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e3:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetTarget(s.gainatktg)
-	e3:SetOperation(s.gainatkop)
-	c:RegisterEffect(e3)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,2))
+	e2:SetCategory(CATEGORY_ATKCHANGE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTarget(s.gainatktg)
+	e2:SetOperation(s.gainatkop)
+	c:RegisterEffect(e2)
 	
 	Nethersea.GenerateToken(c)
 end
-function s.atkcon()
-	return Duel.IsMainPhase()
-end
-function s.atkcheck(c,card)
-	return c:IsCanBeBattleTarget(card)
+function s.atkcheck(c,e)
+	return not e:GetHandler():IsImmuneToEffect(e) and not c:IsImmuneToEffect(e) and e:GetHandler():CanAttack()
 end
 function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return Duel.IsExistingMatchingCard(s.atkcheck,tp,0,LOCATION_MZONE,1,nil,c) and c:IsAttackPos() end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.atkcheck,tp,0,LOCATION_MZONE,1,nil,e) end
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if not c:IsAttackPos() then return end
-	local g=Duel.SelectMatchingCard(tp,s.atkcheck,tp,0,LOCATION_MZONE,1,1,nil,c)
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+		e1:SetValue(800)
+		c:RegisterEffect(e1)
+	end
+	local g=Duel.SelectMatchingCard(tp,s.atkcheck,tp,0,LOCATION_MZONE,1,1,nil,e)
 	if #g>0 then
 		local tg=g:GetFirst()
 		Duel.HintSelection(tg)
@@ -54,7 +58,15 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetCode(EFFECT_NO_BATTLE_DAMAGE)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE)
 		c:RegisterEffect(e1)
-		Duel.ForceAttack(c,tg)
+		Duel.CalculateDamage(c,tg,true)
+		
+		if tg:IsStatus(STATUS_BATTLE_DESTROYED) then
+			Duel.Destroy(tg,REASON_BATTLE)
+		end
+
+		if c:IsStatus(STATUS_BATTLE_DESTROYED) then
+			Duel.Destroy(c,REASON_BATTLE)
+		end
 	end
 end
 function s.gainatktg(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -68,7 +80,7 @@ function s.gainatkop(e,tp,eg,ep,ev,re,r,rp)
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
 		e1:SetValue(800)
 		c:RegisterEffect(e1)
 	end

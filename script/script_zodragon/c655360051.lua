6124f7ea8dce8a6d50382f2e67cfed963bc079d3
พิสิษฐ์ อัครธีรพิศาล <banyspy@gmail.com>
2023-03-06 08:02:58 +0700

    Add Card(in desc)
    Add Zodragon Scorpio

---
+++ script/script_zodragon/c655360051.lua
@@ -0,0 +1,79 @@
+--Zodragon Virgo
+--Scripted by bankkyza
+local s,id=GetID()
+Duel.LoadScript("ZodragonAux.lua")
+function s.initial_effect(c)
+	--Banish
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_REMOVE)
+	e1:SetCountLimit(1,id)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetTarget(s.rmtg)
+	e1:SetOperation(s.rmop)
+	c:RegisterEffect(e1)
+	--Can be activated from the hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_TRAP_ACT_IN_HAND)
+	e2:SetCondition(s.handcon)
+	c:RegisterEffect(e2)
+	--Return from GY
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_TOHAND)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetCondition(s.revcon)
+	e3:SetCost(s.revcost)
+	e3:SetTarget(s.revtg)
+	e3:SetOperation(s.revop)
+	c:RegisterEffect(e3)
+end
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) and chkc:IsAbleToRemove() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+end
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+	end
+end
+function s.handconfilter(c)
+	return c:IsSetCard(SET_ZODRAGON) and c:IsMonster()
+end
+function s.handcon(e)
+	return Duel.IsExistingMatchingCard(s.handconfilter,e:GetHandlerPlayer(),LOCATION_ONFIELD,0,1,nil)
+end
+	--If you Xyz Summon
+function s.revfilter(c,tp)
+	return c:IsSetCard(SET_ZODRAGON) and c:IsSummonType(SUMMON_TYPE_XYZ) and c:IsSummonPlayer(tp)
+end
+function s.revcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.revfilter,1,nil,tp)
+end
+function s.revcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_GRAVE,0,1,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_GRAVE,0,1,1,e:GetHandler())
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function s.revtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToHand() end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+end
+function s.revop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SendtoHand(c,nil,REASON_EFFECT)
+	end
+end

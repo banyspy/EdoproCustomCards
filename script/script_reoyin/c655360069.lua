50b175ca4bb486e333612ec6a43ba64866eb65ca
พิสิษฐ์ อัครธีรพิศาล <banyspy@gmail.com>
2023-03-13 18:56:24 +0700

    Add Card(in desc)
    Add Reoyin x Odd-Eyes Pendulum Dragon

---
+++ script/script_reoyin/c655360069.lua
@@ -0,0 +1,108 @@
+--Reoyin x Odd-Eyes Pendulum Dragon
+--Scripted by bankkyza
+local s,id=GetID()
+--Duel.LoadScript('MagikularAux.lua')
+function s.initial_effect(c)
+	--pendulum summon
+	Pendulum.AddProcedure(c)
+	--destroy and set
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCountLimit(1,{id,0})
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	--tohand
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.tecon)
+	e2:SetTarget(s.tetg)
+	e2:SetOperation(s.teop)
+	c:RegisterEffect(e2)
+	--pierce
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_PIERCE)
+	e3:SetValue(DOUBLE_DAMAGE)
+	c:RegisterEffect(e3)
+	--place in pendulum zone
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e4:SetCode(EVENT_DESTROYED)
+	e4:SetRange(LOCATION_HAND|LOCATION_GRAVE)
+	e4:SetCountLimit(1,{id,2})
+	e4:SetCondition(s.pccon)
+	e4:SetTarget(s.pctg)
+	e4:SetOperation(s.pcop)
+	c:RegisterEffect(e4)
+end
+s.listed_series={0xb04,SET_ODD_EYES}
+function s.thfilter(c)
+	return (c:IsSetCard(SET_ODD_EYES) and c:IsType(TYPE_PENDULUM) or c:IsSetCard(0xb04)) and c:IsMonster() and c:IsAbleToHand()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsControler(tp) and chkc~=e:GetHandler() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceUp,tp,LOCATION_ONFIELD,0,1,e:GetHandler()) 
+	and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,Card.IsFaceUp,tp,LOCATION_ONFIELD,0,1,1,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if c:IsRelateToEffect(e) then
+		if tc:IsRelateToEffect(e) then
+			Duel.Destroy(tc,REASON_EFFECT)
+		end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+		if #g>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
+	end
+end
+function s.tecon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetSummonType()==SUMMON_TYPE_PENDULUM 
+end
+function s.tefilter(c)
+	return c:IsMonster() and c:IsType(TYPE_PENDULUM) and not c:IsForbidden()
+end
+function s.tetg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tefilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil) end
+end
+function s.teop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(24094258,3))
+	local g=Duel.SelectMatchingCard(tp,s.tefilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoExtraP(g,tp,REASON_EFFECT)
+	end
+end
+function s.cfilter(c,tp)
+	return c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_ONFIELD)
+end
+function s.pccon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,nil,tp)
+end
+function s.pctg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.CheckPendulumZones(tp) and c:IsType(TYPE_PENDULUM) and not c:IsForbidden() end
+end
+function s.pcop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not (c:IsRelateToEffect(e) and Duel.CheckPendulumZones(tp)) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+end

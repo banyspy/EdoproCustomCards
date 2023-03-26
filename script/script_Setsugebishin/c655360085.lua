1c4d06bef5299c97025c52665981bdcfe807e322
พิสิษฐ์ อัครธีรพิศาล <banyspy@gmail.com>
2023-03-26 23:00:14 +0700

    Add Card(in desc)
    Add Setsugebishin the Rose Queen and fix text of floral sea abit

---
+++ script/script_Setsugebishin/c655360085.lua
@@ -0,0 +1,80 @@
+--Setsugebishin the Rose Queen
+--Scripted by bankkyza
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special Summon from hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.spcon1)
+	e1:SetTarget(s.sptg1)
+	e1:SetOperation(s.spop1)
+	c:RegisterEffect(e1)
+	--Treated as entire material for queen of the night
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(511001225) -- Specific code for this effect
+	e2:SetOperation(s.tgval)
+	e2:SetValue(2)
+	c:RegisterEffect(e2)
+	--draw
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_DRAW)
+	e3:SetType(EFFECT_TYPE_XMATERIAL+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetProperty(EFFECT_FLAG_EVENT_PLAYER|EFFECT_FLAG_DELAY)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetCondition(s.condition)
+	e3:SetTarget(s.target)
+	e3:SetOperation(s.operation)
+	c:RegisterEffect(e3)
+end
+s.listed_name={89516305}--Queen of the night
+	--If there a monster in the EMZ
+function s.spcon1(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0 or Duel.GetFieldGroupCount(tp,0,LOCATION_EMZONE)>0
+end
+	--Activation legality
+function s.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+	--Special summon from hand
+function s.spop1(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+end
+function s.tgval(e,c)
+	return c:IsCode(89516305)--Queen of the night
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:GetOriginalRace()==RACE_PLANT
+		and not c:IsStatus(STATUS_BATTLE_DESTROYED) and c:GetSummonType(SUMMON_TYPE_XYZ)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.matfilter(c)
+	return c:IsRace(RACE_PLANT) and c:IsMonster()
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	local mat=Duel.GetMatchingGroup(s.matfilter,tp,LOCATION_HAND|LOCATION_GRAVE,0,nil)
+	Duel.Draw(p,d,REASON_EFFECT)
+	if #mat>0 and c:IsRelateToEffect(e) and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+		local tc=Duel.SelectMatchingCard(tp,s.matfilter,tp,LOCATION_HAND|LOCATION_GRAVE,0,1,2,nil)
+		Duel.Overlay(c,tc)
+	end
+end

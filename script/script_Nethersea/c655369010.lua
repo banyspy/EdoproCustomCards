ec4bb23ccb5aa8c6ada45412f0ba3cee01e124c7
พิสิษฐ์ อัครธีรพิศาล <banyspy@gmail.com>
2023-08-09 00:07:00 +0700

    Modify Nethersea Approaching
    Change name and add Fusion effect

--- script/script_Nethersea/c655369010.lua
+++ script/script_Nethersea/c655369010.lua
@@ -5,20 +5,19 @@ Duel.LoadScript("BanyspyAux.lua")
 function s.initial_effect(c)
 	--Also treated as "Umi"
 	Nethersea.AlsoTreatedAsUmi(c)
-	-- Special summon
+	-- Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,{id,0},EFFECT_COUNT_CODE_OATH)
 	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END+TIMING_END_PHASE)
-	e1:SetTarget(s.activatetarget)
-	e1:SetOperation(s.activateoperation)
+	e1:SetTarget(s.acttg)
+	e1:SetOperation(s.actop)
 	c:RegisterEffect(e1)
     --destroy 1 from deck
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetDescription(aux.Stringid(id,3))
 	e2:SetCategory(CATEGORY_DESTROY)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_DELAY)
@@ -37,26 +36,67 @@ function s.initial_effect(c)
     c:RegisterEffect(e4)
 end
 s.listed_names={CARD_UMI}
+function s.acttg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local params = {handler=e:GetHandler(),
+					fusfilter=s.fusfilter,
+					matfilter=aux.FALSE,
+					extrafil=s.fextra,
+					extraop=Fusion.ShuffleMaterial,
+					extratg=s.extratg}
+	local b1=s.sptg(e,tp,eg,ep,ev,re,r,rp,0)
+	local b2=Fusion.SummonEffTG(params)(e,tp,eg,ep,ev,re,r,rp,0)
+	if chk==0 then return b1 or b2 end
+	local op=Duel.SelectEffect(tp,
+		{b1,aux.Stringid(id,1)},
+		{b2,aux.Stringid(id,2)})
+	if op==1 then
+		e:SetCategory(CATEGORY_SPECIAL_SUMMON)
+		s.sptg(e,tp,eg,ep,ev,re,r,rp,1)
+	elseif op==2 then
+		e:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON+CATEGORY_TODECK)
+		Fusion.SummonEffTG(params)(e,tp,eg,ep,ev,re,r,rp,1)
+	end
+	Duel.SetTargetParam(op)
+end
+function s.actop(e,tp,eg,ep,ev,re,r,rp,chk)
+	local op=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
+	local params = {handler=e:GetHandler(),
+					fusfilter=s.fusfilter,
+					matfilter=aux.FALSE,
+					extrafil=s.fextra,
+					extraop=Fusion.ShuffleMaterial,
+					extratg=s.extratg}
+	if op==1 then s.spop(e,tp,eg,ep,ev,re,r,rp)
+	elseif op==2 then Fusion.SummonEffOP(params)(e,tp,eg,ep,ev,re,r,rp) end
+end
 function s.spfilter(c,e,tp)
 	return Nethersea.NetherseaMonsterOrWQ(c) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.activatetarget(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp) 
-	 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE)
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND|LOCATION_GRAVE|LOCATION_REMOVED,0,1,nil,e,tp) 
+	 and Duel.GetMZoneCount(tp)>0 end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND|LOCATION_GRAVE|LOCATION_REMOVED)
 end
-function s.activateoperation(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetMZoneCount(tp)<=0 then return end
 	local c=e:GetHandler()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local sp=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local sp=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_HAND|LOCATION_GRAVE|LOCATION_REMOVED,0,1,1,nil,e,tp)
 	if #sp>0 then Duel.SpecialSummon(sp,0,tp,tp,false,false,POS_FACEUP) end
 end
-function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=e:GetLabelObject()
-	--Duel.Hint(HINT_CARD,0,id)
-	Duel.Destroy(tc,REASON_EFFECT)
+function s.fusfilter(c)
+	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsRace(RACE_AQUA|RACE_THUNDER|RACE_FISH|RACE_SEASERPENT)
 end
+function s.fextra(e,tp,mg)
+	return Duel.GetMatchingGroup(aux.NecroValleyFilter(Fusion.IsMonsterFilter(Card.IsAbleToDeck)),tp,LOCATION_GRAVE|LOCATION_REMOVED,0,nil)
+end
+function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,0,tp,LOCATION_GRAVE+LOCATION_REMOVED)
+end
+
+
+
 function s.gravecon(e,tp,eg,ep,ev,re,r,rp)
 	return (r&REASON_EFFECT)~=0
 end
